5516a0dfa6284f96cb5dbb269aff2bedb01d8df0
karankurbur <karankurbur@gmail.com>
2019-05-04 15:51:14 -0700

    Gets cookies, bookmarks, history*
    History doesn't grab all pages

--- chromium-extension/background.js
+++ chromium-extension/background.js
@@ -1,10 +1,38 @@
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   console.log(request)
-  await chrome.storage.local.set({ privateKey: request.data })
+  if (request.action == 'savePrivateKey') {
+    await chrome.storage.local.set({ privateKey: request.data })
 
-  chrome.storage.local.get(['privateKey'], function(result) {
-    console.log('Value currently is ' + result.privateKey)
-  })
+    chrome.storage.local.get(['privateKey'], function(result) {
+      console.log('Value currently is ' + result.privateKey)
+    })
+  }
 
   sendResponse({ ok: true })
 })
+
+function getBrowsingData() {
+  chrome.history.search({ text: '', maxResults: 3 }, function(data) {
+    // console.log(data.length)
+    data.forEach(function(page) {
+      console.log(page)
+    })
+  })
+}
+
+//getBrowsingData()
+
+function getBookmarks() {
+  chrome.bookmarks.getTree(function(data) {
+    console.log(data)
+  })
+}
+
+function getCookies() {
+  chrome.cookies.getAll({}, function(data) {
+    console.log(data)
+  })
+}
+getCookies()
+
+//getBookmarks()
