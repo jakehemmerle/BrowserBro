3b027e505d9e1aed1eb60078ad6a3d715e960891
Jake Hemmerle <jakehemmerle@gmail.com>
2019-05-04 22:20:44 -0700

    added tests for key generation

--- server/test/package_functionality/keyManagement.tests.js
+++ server/test/package_functionality/keyManagement.tests.js
@@ -1,36 +1,35 @@
-// describe('js-ipfs', () => {
-//   const Ipfs = require('ipfs');
-//   const IPFSNode = new Ipfs();
-//   IPFSNode.on('ready', () => {
-//     console.log('IPFS ready for tests');
-//   });
-//
-//   describe('Key Generation', function() {
-//     it('should generate a key', () => {
-//       const keyParams = {
-//         type: 'rsa',
-//         size: '2048'
-//       };
-//       IPFSNode.key.gen('testKey', keyParams, (err, key) => {
-//         if (err) console.error(e);
-//         console.log(key);
-//       });
-//     });
-//
-//     it('should export that key', () => {
-//     });
-//
-//     it('should delete that key', () => {
-//     });
-//
-//     it('should have no keys', () => {
-//     });
-//
-//     it('should import a new key', () => {
-//     });
-//
-//     it('should rename that key', () => {
-//     });
-//
-//   });
-// });
+describe('js-ipfs', () => {
+  const Ipfs = require('ipfs')
+  const IPFSNode = new Ipfs()
+  IPFSNode.on('ready', () => {
+    console.log('IPFS ready for tests')
+  })
+
+  describe('Key Generation', function () {
+    // it('should generate a key', () => {
+    //   const keyParams = {
+    //     type: 'rsa',
+    //     size: '2048'
+    //   }
+    //   IPFSNode.key.gen('testKey', keyParams, (err, key) => {
+    //     if (err) console.error(e)
+    //     console.log(key)
+    //   })
+    // })
+
+    it('should export that key', () => {
+    })
+
+    it('should delete that key', () => {
+    })
+
+    it('should have no keys', () => {
+    })
+
+    it('should import a new key', () => {
+    })
+
+    it('should rename that key', () => {
+    })
+  })
+})
