d7f9555bae576c023d4934f191f235c01c66f071
Jake Hemmerle <jakehemmerle@gmail.com>
2019-05-11 00:29:12 -0400

    refactored all files and package required

--- src/background.js
+++ src/background.js
@@ -1,3 +1,60 @@
+const IPFS = require('ipfs')
+const bodyParser = require('body-parser')
+const api = require('./api')
+
+// SETUP
+const app = express()
+app.use(bodyParser.json()) // to support JSON-encoded bodies
+const port = 3000
+
+let IPFSNode
+
+const startNode = () => {
+  const options = {
+    config: {
+      Addresses: {
+        Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']
+      }
+    }
+  }
+  IPFSNode = new IPFS(options)
+
+  IPFSNode.on('ready', () => {
+    console.log('IPFS node ready.')
+  })
+}
+
+startNode()
+
+// ENDPOINTS
+app.get('/ping', api.ping)
+
+app.post('/uploadData', async (req, res) => {
+  const browsingData = req.body
+  console.log('Received browsing data')
+
+  const filesAdded = api.uploadToIpfs(IPFSNode, browsingData)
+  res.send({ filesAdded })
+})
+
+app.post('/syncBrowsingData', (req, res) => {
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// add file to ipfs
+
+// add keys to node
+
+// send browsing data to frontend
+
+// save browsing data
+
+
+
+// CLIENT SIDE
+
 browser.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   console.log(request)
   if (request.action === 'savePrivateKey') {
@@ -104,3 +161,4 @@ async function uploadBrowsingData() {
     method: 'POST'
   }).then(res => console.log('response: ', res))
 }
+
