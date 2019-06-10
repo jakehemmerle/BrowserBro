4db428dc80d36aa9968ee8faac3e3356baf8e1ed
Jake Hemmerle <jakehemmerle@gmail.com>
2019-06-10 10:43:48 -0400

    added buttons and blank methods

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,29 @@
 const browser = require('webextension-polyfill')
 
-document.addEventListener('DOMContentLoaded', function () {
-  function handleFiles () {
-    let fileToLoad = this.files[0]
-    var fr = new FileReader()
-    fr.onload = function (e) {
-      console.log(e.target.result)
+const browsingDataToLocalMirror = () => {
 
-      // send to background script here
-      browser.runtime.sendMessage({
-        action: 'savePrivateKey',
-        data: e.target.result
-      })
-    }
-    fr.readAsText(fileToLoad)
+}
 
-    document
-      .getElementById('input')
-      .addEventListener('change', handleFiles, false)
-  }
-})
+const injectTestData = () => {
+
+}
+
+const localMirrorToIPFS = () => {
+
+}
+
+const setNewIPNSLink = () => {
+
+}
+
+const fetchRemoteMirror = () => {
+
+}
+
+const mergeMirrors = () => {
+
+}
+
+const loadMetadataIntoBrowser = () => {
+
+}
