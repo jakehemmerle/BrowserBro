6afe69eea228b01583111fd00a2a36bdf8c6ef54
Jake Hemmerle <jakehemmerle@gmail.com>
2019-07-04 17:42:09 -0400

    added additional crypto methods

--- src/crypto/crypto.js
+++ src/crypto/crypto.js
@@ -3,41 +3,55 @@ const generateRSAKeyPair = () => {
 }
 
 const generateSessionKey = () => {
-  // TODO: logic for generateSessionKey
+  // TODO: logic for generateSessionKey (AES)
 }
 
-const addMasterKey = () => {
-  // TODO: logic for addMasterKey
+const setMasterKey = (publicKey, privateKey) => {
+  // TODO: logic for setMasterKey, master RSA
+}
+
+const setSessionKey = (newSessionKey) => {
+  // TODO: logic for setSessionKey, set key used to encrypt data
 }
 
 const deleteMasterKey = () => {
   // TODO: logic for deleteMasterKey
 }
 
-const signDeviceKey = () => {
+const signDeviceKey = (devicePublicKey) => {
   // TODO: logic for signDeviceKey
 }
 
-const verifyDeviceKey = () => {
+const verifyDeviceKey = (devicePublicKey) => {
   // TODO: logic for verifyDeviceKey
 }
 
-const encryptData = () => {
+const encryptData = (data) => {
   // TODO: logic for encryptData
 }
 
-const decryptData = () => {
+const decryptData = (data) => {
   // TODO: logic for decryptData
+}
+
+const encryptSymmetricKeyForEachDevice = () => {
+  // TODO: logic for encryptSymmetricKeys
+}
+
+const getSymmetricKey = () => {
+  // TODO: logic for getSymmetricKey, decrypt and returns the symmetric key
 }
 
 module.exports = {
   generateRSAKeyPair,
   generateSessionKey,
-  addMasterKey,
+  setMasterKey,
+  setSessionKey,
   deleteMasterKey,
   signDeviceKey,
   verifyDeviceKey,
   encryptData,
-  decryptData
+  decryptData,
+  encryptSymmetricKeyForEachDevice,
+  getSymmetricKey
 }
-
